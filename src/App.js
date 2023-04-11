cf8bd6ee9dfaa41e456e41a89482444cd8f5f911
trunghaiii <haitran022001@gmail.com>
2023-04-10 21:38:41 -0400

    Config redux toolkit in react app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './redux/slices/counterSlice'
 
 function App() {
+  const dispatch = useDispatch()
+
+  const count = useSelector(state => state.counter.value)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h1>Hello world with React and Trung Hai!</h1>
+        <button onClick={() => dispatch(increment())}>Increase</button>
+        <button onClick={() => dispatch(decrement())}>Decrease</button>
+        <br />
+        <div>Count = {count}</div>
       </header>
     </div>
   );
